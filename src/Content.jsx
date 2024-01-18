fab37c9624c04abe9cc6cbc69219ec1daa974373
BuickSalesman <adam.odonnell.e@gmail.com>
2024-01-17 20:23:43 -0500

    Initial commit

---
+++ src/Content.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+export function Content() {
+  const [plant, setPlant] = useState({});
+
+  useEffect(() => {
+    console.log("Put request here", plant);
+    setPlant({
+      name: "Plant",
+      id: 1,
+    });
+  }, []);
+
+  console.log("Plant", plant);
+
+  return (
+    <div>
+      <h1>{plant.name}</h1>
+    </div>
+  );
+}
+
+// file upload box
