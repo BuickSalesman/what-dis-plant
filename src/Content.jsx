01f0f4e4aa981c90b2a92d1e7b93ce9b862ec79e
BuickSalesman <adam.odonnell.e@gmail.com>
2024-01-23 19:34:49 -0500

    added axios requests

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,22 +1,49 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 export function Content() {
   const [plant, setPlant] = useState({});
 
   useEffect(() => {
-    console.log("Put request here", plant);
-    setPlant({
-      name: "Plant",
-      id: 1,
-    });
+    const accessToken = "NGWphc2bBSqgWV6";
+    const apiUrl = `https://plant.id/api/v3/identification/${accessToken}`;
+
+    const params = {
+      details:
+        "common_names,url,description,taxonomy,rank,gbif_id,inaturalist_id,image,synonyms,edible_parts,watering,propagation_methods",
+      language: "en",
+    };
+
+    const headers = {
+      "Api-Key": "DBtTY2wyOYZeqs7L1naVhRKNR9HPlyA7AamvpkSPoWjndd7gwI",
+    };
+
+    axios
+      .get(apiUrl, {
+        headers,
+        params,
+      })
+      .then((response) => {
+        setPlant(response.data.result.classification.suggestions[0].details);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        // Handle errors here
+        console.error(error);
+      });
   }, []);
 
   console.log("Plant", plant);
+  console.log("Details", plant?.common_names[0]);
 
   return (
     <div>
-      <h1>{plant.name}</h1>
-      <h1>{plant.id}</h1>
+      {plant && (
+        <>
+          <h1>{plant.common_names}</h1>
+          <img src={plant.image.value} />
+        </>
+      )}
     </div>
   );
 }
