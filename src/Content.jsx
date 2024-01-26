58a82f56bc6796be750b74eddca1a83ae5a7cad6
BuickSalesman <adam.odonnell.e@gmail.com>
2024-01-26 18:44:24 -0500

    Debugging line 41, will resume 1/28

--- src/Content.jsx
+++ src/Content.jsx
@@ -25,7 +25,7 @@ export function Content() {
       })
       .then((response) => {
         setPlant(response.data.result.classification.suggestions[0].details);
-        console.log(response.data);
+        console.log(response.data.result.classification.suggestions[0].details);
       })
       .catch((error) => {
         // Handle errors here
@@ -33,8 +33,8 @@ export function Content() {
       });
   }, []);
 
-  console.log("Plant", plant);
-  console.log("Details", plant?.common_names[0]);
+  // console.log("Plant", plant);
+  // console.log("Details", plant?.common_names[0]);
 
   return (
     <div>
