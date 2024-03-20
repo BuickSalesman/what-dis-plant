b61bd9ba3f9e8ae6c57e6df5ce9d11a30a08945a
BuickSalesman <adam.odonnell.e@gmail.com>
2024-03-20 08:20:23 -0400

    messing with styling

--- src/Content.jsx
+++ src/Content.jsx
@@ -2,13 +2,15 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { Card } from "flowbite-react";
 
+const accessTokens = ["NbssSTEtdUABkUN", "SS4hTbbCJ0LveRJ", "vWwySsSLymlsRur"];
+
 export function Content() {
   const [plant, setPlant] = useState({});
 
   const apiKey = "DBtTY2wyOYZeqs7L1naVhRKNR9HPlyA7AamvpkSPoWjndd7gwI";
 
   useEffect(() => {
-    const accessToken = "SS4hTbbCJ0LveRJ";
+    const accessToken = "NbssSTEtdUABkUN";
     const apiUrl = `https://plant.id/api/v3/identification/${accessToken}`;
 
     const params = {
@@ -27,8 +29,8 @@ export function Content() {
         params,
       })
       .then((response) => {
+        console.log(response);
         setPlant(response.data.result.classification.suggestions[0]);
-        // console.log(response.data.result.classification.suggestions[0].details);
         console.log(response.data.result.classification.suggestions[0]);
       })
       .catch((error) => {
@@ -38,13 +40,13 @@ export function Content() {
 
   async function handleImageUpload(event) {
     event.preventDefault();
-    // console.log("event", event.target[0].files);
+    console.log("event", event.target[0].files);
 
     const filesObject = event.target[0].files;
     const files = Object.values(filesObject);
 
     const base64Files = await convertImageToBase64(files);
-    // console.log(base64Files);
+    console.log(base64Files);
 
     axios
       .post(
@@ -83,21 +85,39 @@ export function Content() {
   }
 
   return (
-    <Card href="#" className="max-w-sm" imgAlt="dis plant is da greatest" imgSrc={plant.details.image.value} horizontal>
-      <div className="w-screen">
-        {Object.keys(plant).length > 0 && (
-          <div className="flex flex-col justify-center">
-            <form onSubmit={handleImageUpload}>
-              <input type="file" multiple />
-              <button type="submit" className="bg-green-500 text-white px-4 py-2 rounded border">
-                what-dis-plant?
-              </button>
-            </form>
-            <h1 className="bg-red-500 text-center">{plant.name}</h1>
-            {/* <img className="p-20" src={plant.details.image.value} /> */}
+    <div>
+      {Object.keys(plant).length > 0 && (
+        <>
+          <div className="w-screen">
+            <div className="flex flex-col justify-center">
+              <form onSubmit={handleImageUpload}>
+                <input type="file" multiple />
+                <button type="submit" className="bg-green-500 text-white px-4 py-2 rounded border">
+                  what-dis-plant?
+                </button>
+              </form>
+              <h1 className="bg-red-500 text-center">{plant.name}</h1>
+            </div>
           </div>
-        )}
-      </div>
-    </Card>
+
+          {/* <Card
+            href="#"
+            className="max-w-sm"
+            imgAlt="dis plant is da greatest"
+            imgSrc={plant.details.image.value}
+            horizontal
+          ></Card> */}
+
+          <Card className="max-w-sm" imgSrc={plant.details.image.value} horizontal>
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Noteworthy technology acquisitions 2021
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+            </p>
+          </Card>
+        </>
+      )}
+    </div>
   );
 }
